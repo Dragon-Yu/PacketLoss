114c184fc856b7016efbaafc0782f01ee5473b4f
DillionApple <DillionMango@gmail.com>
2016-10-03 14:01:53 +0800

    seprate some functions to funcs/ and add some decorators

--- packet_loss_project/account/tests.py
+++ packet_loss_project/account/tests.py
@@ -16,7 +16,7 @@ class LoginTest(TestCase):
         data['username'] = 'user1'
         data['password'] = '1234567890'
         json_data = json.dumps(data)
-        response = self.client.post(reverse('login'), json_data, content_type="applicaton/json")
+        response = self.client.post(reverse('login'), json_data, content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['success'], 'true')
         self.assertEqual(response.json()['data'], {})
@@ -33,9 +33,45 @@ class LoginTest(TestCase):
         data['username'] = 'user1'
         data['password'] = '12345678'
         json_data = json.dumps(data)
-        response = self.client.post(reverse('login'), json_data, content_type="applicaton/json")
+        response = self.client.post(reverse('login'), json_data, content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['success'], 'false')
         self.assertEqual(response.json()['data'], {})
         self.assertEqual(response.json()['msg'], 'Password incorrect')
         
+class RegisterTest(TestCase):
+
+    def test_valid_register(self):
+        """
+        ensure register returns correct json format data if register information is valid
+        """
+        data = {}
+        data['username'] = 'user1'
+        data['password'] = '1234567890'
+        json_data = json.dumps(data)
+        response = self.client.post(reverse('register'), json_data, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['success'], 'true')
+        self.assertEqual(response.json()['data'], {})
+        self.assertEqual(response.json()['msg'], 'Register success')
+
+    def test_invalid_register(self):
+        """
+        test several invalid register cases:
+        1. user already exist
+        2. username not valid
+        3. userpassword not valid
+        """
+
+        # test case 1
+        user1 = User(username='user1')
+        user1.set_password('1234567890')
+        user1.save()        
+        data = {}
+        data['username'] = 'user1'
+        data['password'] = '12345678'
+        json_data = json.dumps(data)
+        response = self.client.post(reverse('register'), json_data, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['success'], 'false')
+        # [TODO] test case 2 & test case 3
--- packet_loss_project/account/views.py
+++ packet_loss_project/account/views.py
@@ -1,45 +1,16 @@
-import json
-
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-def _get_received_data(request, *args):
-    """
-    check request format and return a dict from json data.
-    1. check it's content type is "applicaton/json"
-    2. check every field in args is in the json data
-    """ 
-    if (request.META.get('CONTENT_TYPE') != "applicaton/json"):
-        return None
+from account.forms import UserForm
+from funcs.json_response import *
+from funcs.decorators import load_json_data, login_required
 
-    try:
-        received_data = json.loads(request.body)
-        for key in args:
-            received_data[key]
-    except:
-        return None
-    else:
-        return received_data
-
-def _formatted_json_response(success="false", data={}, msg=""):
-    d = {'success':success,
-         'data':data,
-         'msg':msg,}
-    return JsonResponse(d)
-
-def _false_json_response(msg=""):
-    return _formatted_json_response(success="false", data={}, msg=msg)
-
-def _true_json_response(data={}, msg=""):
-    return _formatted_json_response(success="true", data=data, msg=msg)
-
-def user_login(request):
-    received_data = _get_received_data(request, 'username', 'password')
-    if (not received_data):
-        return _false_json_response(msg="Invalid json format")
+@load_json_data('username', 'password')
+def user_login(request, received_data={}):
+    # [DONE] use decorator to replace this kind of duplicated code
     username = received_data.get('username')
     password = received_data.get('password')
 
@@ -47,19 +18,33 @@ def user_login(request):
     if user:
         if user.is_active:
             login(request, user)
-            return _true_json_response(msg= "Login success")
+            return true_json_response(msg= "Login success")
         else:
-            return _false_json_response(msg="Your account is disabled")
+            return false_json_response(msg="Your account is disabled")
     else:
         try:
             User.objects.get(username=username)
         except User.DoesNotExist:
-            return _false_json_response(msg="No such user")
+            return false_json_response(msg="No such user")
         else:
-            return _false_json_response(msg="Password incorrect")
+            return false_json_response(msg="Password incorrect")
     
 
+@load_json_data('username', 'password')
+def register(request, received_data={}):
+    # [DONE] validate the username and password format
+    user_form = UserForm(data=received_data)
+    if user_form.is_valid():
+        user = user_form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        return true_json_response(msg="Register success")
+    else:
+        error_dict = json.loads(user_form.errors.as_json())
+        return false_json_response(data=error_dict, msg="Register fail")
 
-def register(request):
-    pass
-    
+@login_required
+def user_logout(request):
+    # [NOTICE] this is not a defined API
+    logout(request)
+    return true_json_response(msg="Logout success")     
