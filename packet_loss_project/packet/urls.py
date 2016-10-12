77b8c545e8b0f74057fe5b78c8b6f74f4bc09fe4
DillionApple <DillionMango@gmail.com>
2016-10-12 13:10:27 +0800

    fix tow small bugs

--- packet_loss_project/packet/urls.py
+++ packet_loss_project/packet/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^redrop/$', views.redrop, name="redrop"),
     url(r'^details/$', views.get_packet_details, name="get_packet_details"),
     url(r'^list_owning_packets/$', views.get_owning_packets, name="get_owning_packets"),
-    url(r'^list_owned_packets$', views.get_owned_packets, name="get_owned_packets"),
+    url(r'^list_owned_packets/$', views.get_owned_packets, name="get_owned_packets"),
     url(r'^ignore/$', views.ignore, name="ignore"),
 ]
